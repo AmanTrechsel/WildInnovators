07d961b9610a45da15f475ca71ff4e4f284de3d2
AmanTrechsel <aman.trechsel@student.nhlstenden.com>
2023-05-12 09:38:45 +0200

    Added an ID to Subject class

--- TimeWise/Assets/Scripts/Subject.cs
+++ TimeWise/Assets/Scripts/Subject.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Subject", menuName = "Subject", order = 1)]
 public class Subject : ScriptableObject
 {
+  public uint id;
   public string name;
   public List<ARItem> items;
 }
