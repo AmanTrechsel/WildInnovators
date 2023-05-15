7d7b77abd228f1062a386bd40f55e8fa789cc5b8
jariolyslager <jari.olijslager@student.nhlstenden.com>
2023-05-15 11:12:47 +0200

    Changed the button functionality to now work even better

--- TimeWise/Assets/Scripts/MenuManager.cs
+++ TimeWise/Assets/Scripts/MenuManager.cs
@@ -6,19 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-  // Gets the index of the child within the parent object "MenuButtons".
-  public void GetIndex()
+  // Method to select the subject
+  public void SetSubjectByIndex(int index)
   {
-      int index = transform.GetSiblingIndex();
-      AppManager.Instance.arSubject = ResourceManager.Instance.GetSubjectByID(index);
-      Debug.Log(index);
+    AppManager.Instance.arSubject = ResourceManager.Instance.GetSubjectByID(index);
   }
 
   // Switches to the ARScene, which will contain the subject selected at the function above here.
   public void GoToARScene()
   {
-      SceneManager.LoadScene("AR");
-      Debug.Log(AppManager.Instance.arSubject);
+    SceneManager.LoadScene("AR");
   }
 
   
