6417527cbff4a1fa6fc4f45131791b4bc05231f9
jariolyslager <jari.olijslager@student.nhlstenden.com>
2023-05-24 15:04:48 +0200

    Added the button for settings-page, homepage and encyclopedia-page

--- TimeWise/Assets/Scripts/MenuManager.cs
+++ TimeWise/Assets/Scripts/MenuManager.cs
@@ -17,16 +17,4 @@ public class MenuManager : MonoBehaviour
   {
     SceneManager.LoadScene("AR");
   }
-  
-  // Function for the settingsbutton
-  public void GoToSettings()
-  {
-    SceneManager.LoadScene("Settings");
-  }
-
-  // Function for getting to the Encyclopedia
-  public void GoToEncyclopedia()
-  {
-    SceneManager.LoadScene("Encyclopedia");
-  }
 }
