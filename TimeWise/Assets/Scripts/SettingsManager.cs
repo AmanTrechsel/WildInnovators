21deb5f4ad1b15c83a6e959ca1b9774a768ce3d3
jariolyslager <jari.olijslager@student.nhlstenden.com>
2023-05-29 07:29:27 +0200

    Wrote the code for saving the settings and started on making settingspage modular

--- TimeWise/Assets/Scripts/SettingsManager.cs
+++ TimeWise/Assets/Scripts/SettingsManager.cs
@@ -4,9 +4,44 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Localization.Settings;
+using System.IO;
 
 public class SettingsManager : MonoBehaviour
 {
+
+  // For saving data in the cloud
+  private bool permission = true;
+  private int languageIndex;
+
+  public void SaveData()
+  {
+    SettingsData data = new SettingsData();
+    data.permission = permission;
+    data.languageIndex = languageIndex;
+    
+    string json = JsonUtility.ToJson(data);
+    File.WriteAllText(Application.dataPath+"/DataFile.json", json);   // Path might need to be changed, because this needs to be uploaded to the cloud.
+  }
+
+  public void LoadData()
+  {
+    string json = File.ReadAllText(Application.dataPath+"/DataFile.json");  // Path then also needs to be changed here.
+    SettingsData data = JsonUtility.FromJson<SettingsData>(json);
+
+    permission = data.permission;
+    languageIndex = data.languageIndex;
+  }
+
+  // Probably want the settings to load from the homescreen so this probably needs to happen in AppManager.cs.
+  void awake()
+  {
+    LoadData();
+  }
+
+  // Also .json isn't the most secure format, might want to change it to a binary file or something like that.
+
+
+
   // For the permission setting
   [SerializeField]
   private Button button;
@@ -22,16 +57,20 @@ public class SettingsManager : MonoBehaviour
     if(button.image.sprite == toggleOn)
     {
       button.image.sprite = toggleOff;
+      permission = false;
     }
     else if(button.image.sprite == toggleOff)
     {
       button.image.sprite = toggleOn;
+      permission = true;
     }
   }
 
+  // For the language setting
   public void ChangeLanguage(int ID)
   {
     StartCoroutine(SetLanguage(ID));
+    languageIndex = ID;
   }
 
   IEnumerator SetLanguage(int index)
