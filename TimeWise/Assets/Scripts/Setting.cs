c0f136b0c5567773c7203b1f3d9cf102fa93e06d
jariolyslager <jari.olijslager@student.nhlstenden.com>
2023-05-30 10:23:58 +0200

    Added the files for the modular settingspage

---
+++ TimeWise/Assets/Scripts/Setting.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Setting", menuName = "ScriptableObjects/Setting", order = 1)]
+public class Setting : ScriptableObject
+{
+  public uint id;
+  public string name;
+  public Sprite buttonGraphic;
+  public string title;
+  public List<ARItem> items;
+  public GameObject content;
+}
