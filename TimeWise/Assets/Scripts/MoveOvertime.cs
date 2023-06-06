c694f66754ac05c2e72a12b818047f783a564194
jariolyslager <jari.olijslager@student.nhlstenden.com>
2023-06-06 13:17:19 +0200

    Randomized movement in MoveOvertime.cs

--- TimeWise/Assets/Scripts/MoveOvertime.cs
+++ TimeWise/Assets/Scripts/MoveOvertime.cs
@@ -16,11 +16,29 @@ public class MoveOvertime : MonoBehaviour
   {
     // Move and rotate the object over time
     MoveObject(moveSpeed, rotationSpeed);
+    StartCoroutine(RandomizeValues());
   }
 
   public void MoveObject(Vector3 moveSpeed, Vector3 rotationSpeed)
   {
     transform.Translate(moveSpeed * Time.deltaTime);
     transform.Rotate(rotationSpeed * Time.deltaTime);
+  }
+
+  IEnumerator RandomizeValues()
+  {
+    yield return new WaitForSeconds(Random.Range(10.0f, 50.0f));
+
+    int randomValue = Random.Range(0, 1);
+
+    if(randomValue == 0)
+    {
+      moveSpeed.x += Random.Range(-10.0f, 10.0f);
+      moveSpeed.z += Random.Range(-10.0f, 10.0f);
+    }
+    else if(randomValue == 1)
+    {
+      rotationSpeed.y += Random.Range(-50.0f, 50.0f);
+    }
   }
 }
