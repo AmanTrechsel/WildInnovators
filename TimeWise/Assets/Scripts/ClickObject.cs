2d54f4601caaad5b20415072dc456994003a9b94
saulewestra <saule.westra@student.nhlstenden.com>
2023-06-07 12:58:31 +0200

    First part for clicking an object.

--- TimeWise/Assets/Scripts/ClickObject.cs
+++ TimeWise/Assets/Scripts/ClickObject.cs
@@ -1,63 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
+
+    /* Copyright Xenfinity LLC - All Rights Reserved
+     * Written by Bilal Itani
+     * https://www.youtube.com/watch?v=EANtTI6BCxk
+     */
 
 public class ClickObject : MonoBehaviour
 {
-  public GameObject model;
-
-  void Update()
-  {
-    if (Input.GetMouseButtonDown(0)) { 
-        if (model == getClickedObject(out RaycastHit hit))
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
         {
-          print("clicked/ object is touched"); 
+            RaycastHit hit;
+            // This will turn a screen into a ray
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // hit is used to send information into a variable without returning
+            // out will store the information from this ray cast into the other ray cast
+            // 100.0f is the border how far the sufficient is
+            if(Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if(hit.transform != null)
+                {
+                    PrintName(hit.transform.gameObject);
+                }
+            }
         }
-        else if (model != getClickedObject(out RaycastHit hit2))
-        {
-          print("bla");
-        }
+    }
+    
+    void PrintName(GameObject ball)
+    {
+        print(ball.name);
     }
 
-    if (Input.GetMouseButtonUp(0)) { print("click is off"); }
-  }
-
-  // Mark - Get clicked object 
-  // https://www.youtube.com/watch?v=pfkQDGhd8_A&t=63s
-
-  GameObject getClickedObject (out RaycastHit hit)
-  {
-    GameObject target = null;
-
-    Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-
-    if (Physics.Raycast(ray.origin, ray.direction = new Vector3(0f, 0f, 10f), out hit))
-    {
-      if (!isPointerOverUIObject()) { target = hit.collider.gameObject; }
-    }
-    return target;
-  }
-
-  private bool isPointerOverUIObject()
-  {
-    bool check;
-
-    PointerEventData ped = new PointerEventData(EventSystem.current);
-    ped.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-    List<RaycastResult> results = new List<RaycastResult>();
-    EventSystem.current.RaycastAll(ped, results);
-
-    if (results.Count == 0)
-    {
-        check = false;
-    } 
-    else
-    {
-        check = true;
-    }
-
-    return check;
-  }
 }
