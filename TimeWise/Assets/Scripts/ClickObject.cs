ac8fe07a6d39e3e4bfd54a4612116b45a5bca497
jariolyslager <jari.olijslager@student.nhlstenden.com>
2023-06-08 13:15:41 +0200

    Edited ClickObject.cs, however it still doesn't quite work

--- TimeWise/Assets/Scripts/ClickObject.cs
+++ TimeWise/Assets/Scripts/ClickObject.cs
@@ -1,39 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-    /* Copyright Xenfinity LLC - All Rights Reserved
-     * Written by Bilal Itani
-     * https://www.youtube.com/watch?v=EANtTI6BCxk
-     */
+using System.IO;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using TMPro;
 
 public class ClickObject : MonoBehaviour
 {
   [SerializeField]
-  private Camera rayCamera;
+  private ARRaycastManager raycastManager;
+  // [SerializeField]
+  // private Camera rayCamera;
   [SerializeField]
   private GameObject question;
 
   void Update()
   {
-    if(Input.GetTouch(0).tapCount > 0)
+    /*if(Input.GetTouch(0).phase == TouchPhase.Began)
     {
-      RaycastHit hit;
+      Touch touch = Input.GetTouch(0);
+      // RaycastHit hit;
       // This will turn a screen into a ray
-      Ray ray = rayCamera.ScreenPointToRay(Input.GetTouch(0).position);
+      Ray ray = rayCamera.ScreenPointToRay(touch.rawPosition);
 
       // hit is used to send information into a variable without returning
       // out will store the information from this ray cast into the other ray cast
       // 100.0f is the border how far the sufficient is
       if(Physics.Raycast(ray, out hit, 100.0f))
       {
-        if(hit.transform != null)
-        {
+        // if(hit.transform != null)
+        // {
           question.SetActive(true);
-        }
+        // }
+      }
+    }*/
+
+    if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+    {
+      List<ARRaycastHit> hits = new List<ARRaycastHit>();
+      raycastManager.Raycast(Input.GetTouch(0).position, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+      if (hits.Count > 0)
+      {
+          question.SetActive(true);
       }
     }
   }
-    
-  // setActive is going to be used here
 }
