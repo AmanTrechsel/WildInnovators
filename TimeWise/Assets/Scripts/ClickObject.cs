1838c16f7cb7ec6a9cec79d38dd0449f868fbe2b
jariolyslager <jari.olijslager@student.nhlstenden.com>
2023-06-08 11:34:02 +0200

    Changed ClickObject.cs to now use touch instead of mouseclicks

--- TimeWise/Assets/Scripts/ClickObject.cs
+++ TimeWise/Assets/Scripts/ClickObject.cs
@@ -9,26 +9,31 @@ using UnityEngine;
 
 public class ClickObject : MonoBehaviour
 {
-    void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hit;
-            // This will turn a screen into a ray
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+  [SerializeField]
+  private Camera rayCamera;
+  [SerializeField]
+  private GameObject question;
 
-            // hit is used to send information into a variable without returning
-            // out will store the information from this ray cast into the other ray cast
-            // 100.0f is the border how far the sufficient is
-            if(Physics.Raycast(ray, out hit, 100.0f))
-            {
-                if(hit.transform != null)
-                {
-                    // [GameObject].SetActive(true);
-                }
-            }
+  void Update()
+  {
+    if(Input.GetTouch(0).tapCount > 0)
+    {
+      RaycastHit hit;
+      // This will turn a screen into a ray
+      Ray ray = rayCamera.ScreenPointToRay(Input.GetTouch(0).position);
+
+      // hit is used to send information into a variable without returning
+      // out will store the information from this ray cast into the other ray cast
+      // 100.0f is the border how far the sufficient is
+      if(Physics.Raycast(ray, out hit, 100.0f))
+      {
+        if(hit.transform != null)
+        {
+          question.SetActive(true);
         }
+      }
     }
+  }
     
-    // setActive is going to be used here
+  // setActive is going to be used here
 }
