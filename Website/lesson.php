3b80796659ce8ad9f45214a667b9f0aa379c1511
BrandonNijveld <brandon.nijveld@student.nhlstenden.com>
2023-06-12 11:31:26 +0200

    possibly fixed lesson page php issues

--- Website/lesson.php
+++ Website/lesson.php
@@ -73,10 +73,27 @@
                         <label for="groups">Kies een groep:</label>
                         <select name="groups" id="groups">
                             <?php
+                            try
+                            {
+                                $dbHandler = new PDO ("mysql:host={$dbhost};dbname={$dbname};charset=utf8;", "{$dbuser}", "{$dbpassword}");
+                                
+                                try
+                                {
                                 $stmt = $dbHandler->prepare("SELECT groupId FROM `groups` WHERE `username` = :username");
                                 $stmt->bindParam("username", $username, PDO::PARAM_STR);
                                 $stmt->execute();
                                 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                                }
+                                catch (Exception $ex)
+                                {
+                                    $errors[] = $ex->getMessage();
+                                }
+
+                            }
+                            catch(Exception $ex)
+                            {
+                                $errors[] = $ex->getMessage();
+                            }
 
                                 foreach($results as $result){
                                     echo "<option value='groupID' id='groupID'>$result</option>";
