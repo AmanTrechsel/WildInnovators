9795d9c7dfa10d07a342b3c1db597ab933364a2b
jariolyslager <jari.olijslager@student.nhlstenden.com>
2023-06-13 13:25:06 +0200

    Did some preparations for using touch to make AR objects interactable

--- TimeWise/Assets/Scripts/ClickObject_testcase2.cs
+++ TimeWise/Assets/Scripts/ClickObject_testcase2.cs
@@ -7,31 +7,33 @@ public class ClickObject_testcase2 : MonoBehaviour
     [SerializeField]
     private Camera cam;
 
+    [SerializeField]
+    private GameObject question;
+
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetTouch(0).phase == TouchPhase.Began)       // Input.GetMouseButtonDown(0)
         {
             RaycastHit hit;
-            // This will turn a screen into a ray
 
-            // beter een private camera hier zetten
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            Ray ray = cam.ScreenPointToRay(Input.GetTouch(0).position);     // Input.mousePosition
 
-            // hit is used to send information into a variable without returning
-            // out will store the information from this ray cast into the other ray cast
-            // 100.0f is the border how far the sufficient is
             if(Physics.Raycast(ray, out hit, 10000.0f))
             {
                 if(hit.transform != null)
                 {
                     PrintName(hit.transform.gameObject);
+                    question.SetActive(true);
                 }
             }
         }
     }
     
+    // Method will be obsolete in the final version
     void PrintName(GameObject ball)
     {
         print(ball.name);
     }
+
+    // The question is now hard-coded into the scene (it's already in the hierarchy), but we want the content of question change depending on which object has been clicked.
 }
