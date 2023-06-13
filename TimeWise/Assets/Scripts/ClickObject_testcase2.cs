cbbd0c64500207bce158d32ccaa41b5249283e98
saulewestra <saule.westra@student.nhlstenden.com>
2023-06-13 09:53:05 +0200

    Raycast test on 3D models with functioning clickable method.
    Conclusion the prefabs needs to contain a (spehere) collider.

--- TimeWise/Assets/Scripts/ClickObject_testcase2.cs
+++ TimeWise/Assets/Scripts/ClickObject_testcase2.cs
@@ -20,7 +20,7 @@ public class ClickObject_testcase2 : MonoBehaviour
             // hit is used to send information into a variable without returning
             // out will store the information from this ray cast into the other ray cast
             // 100.0f is the border how far the sufficient is
-            if(Physics.Raycast(ray, out hit, 100.0f))
+            if(Physics.Raycast(ray, out hit, 10000.0f))
             {
                 if(hit.transform != null)
                 {
