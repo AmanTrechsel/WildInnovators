75de03770d066e4e08353638fcdfdedb782c53fc
saulewestra <saule.westra@student.nhlstenden.com>
2023-06-13 09:15:59 +0200

    Enabeled all the objects

--- TimeWise/Assets/Scripts/ClickObject_testcase2.cs
+++ TimeWise/Assets/Scripts/ClickObject_testcase2.cs
@@ -4,33 +4,34 @@ using UnityEngine;
 
 public class ClickObject_testcase2 : MonoBehaviour
 {
-  private Camera camera;
+    [SerializeField]
+    private Camera cam;
 
-  void Update()
-  {
-    if(Input.GetMouseButtonDown(0))
+    void Update()
     {
-      RaycastHit hit;
-      // This will turn a screen into a ray
-
-      // beter een private camera hier zetten
-      Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-
-      // hit is used to send information into a variable without returning
-      // out will store the information from this ray cast into the other ray cast
-      // 100.0f is the border how far the sufficient is
-      if(Physics.Raycast(ray, out hit, 100.0f))
-      {
-        if(hit.transform != null)
+        if(Input.GetMouseButtonDown(0))
         {
-          PrintName(hit.transform.gameObject);
-        }
-      }
-    }
+            RaycastHit hit;
+            // This will turn a screen into a ray
 
-    void PrintName(GameObject model)
-    {
-      print(model.name);
+            // beter een private camera hier zetten
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            // hit is used to send information into a variable without returning
+            // out will store the information from this ray cast into the other ray cast
+            // 100.0f is the border how far the sufficient is
+            if(Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if(hit.transform != null)
+                {
+                    PrintName(hit.transform.gameObject);
+                }
+            }
+        }
     }
-  }
+    
+    void PrintName(GameObject ball)
+    {
+        print(ball.name);
+    }
 }
