fd1a71f583a7e03e590d911c8f52756c741e5337
AmanTrechsel <aman.trechsel@student.nhlstenden.com>
2023-06-16 09:52:58 +0200

    Added webeditor to lesson.php

--- Website/lesson.php
+++ Website/lesson.php
@@ -109,6 +109,39 @@
                 <div class="loginprompt">
                     <h4>Upload je Modellen</h4>
                 </div>
+                <div class="webeditor" style="text-align: center; padding: 0; border: 0; margin: 0">
+                    <canvas id="unity-canvas" width=1200 height=800 style="width: 1200px; height: 800px; background: #FF8800"></canvas>
+                    <script src="WebEditor/Build/1.1.0.loader.js"></script>
+                    <script>
+                        if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+                            // Mobile device style: fill the whole browser client area with the game canvas:
+                            var meta = document.createElement('meta');
+                            meta.name = 'viewport';
+                            meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
+                            document.getElementsByTagName('head')[0].appendChild(meta);
+
+                            var canvas = document.querySelector("#unity-canvas");
+                            canvas.style.width = "50%";
+                            canvas.style.height = "50%";
+                            canvas.style.position = "fixed";
+
+                            document.body.style.textAlign = "left";
+                        }
+
+                        createUnityInstance(document.querySelector("#unity-canvas"), {
+                            dataUrl: "WebEditor/Build/f6e885ef6f053eabac2c1324ce768fac.data",
+                            frameworkUrl: "WebEditor/Build/05262282d3d6f17fceaef00bac0bcb70.js",
+                            codeUrl: "WebEditor/Build/350ddff92850a7af495559905791fa58.wasm",
+                            streamingAssetsUrl: "WebEditor/StreamingAssets",
+                            companyName: "WildInnovators",
+                            productName: "WebEditor",
+                            productVersion: "1.1.0",
+                            // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
+                            // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
+                        });
+                    </script>
+                </div>
+                <br><br><br><br><br><br>
                 <div class="loginprompt">
                     <input type="submit" id="createButton" name="createGroup" value="Creëer">
                 </div>
